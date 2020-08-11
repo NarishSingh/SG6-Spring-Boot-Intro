8a570acc578c27b5660b3cd6af367e2bc001170d
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-08-11 12:56:25 -0400

    Code along completed

--- JDBCExampleStarter/src/main/java/com/sg/jdbcexample/ToDoListMain.java
+++ JDBCExampleStarter/src/main/java/com/sg/jdbcexample/ToDoListMain.java
@@ -1,7 +1,11 @@
 package com.sg.jdbcexample;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 import javax.sql.DataSource;
 
@@ -12,10 +16,19 @@ import javax.sql.DataSource;
 public class ToDoListMain {
 
     private static Scanner sc;
+    private static DataSource ds;
 
     public static void main(String[] args) {
-
         sc = new Scanner(System.in);
+
+        //connect to db - todoDB
+        try {
+            ds = getDataSource();
+        } catch (SQLException e) {
+            System.out.println("Error connecting");
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
 
         do {
             System.out.println("To-Do List");
@@ -53,8 +66,8 @@ public class ToDoListMain {
 
         } while (true);
     }
-    
-    private static DataSource getDataSource() throws SQLException{
+
+    private static DataSource getDataSource() throws SQLException {
         MysqlDataSource ds = new MysqlDataSource();
         ds.setServerName("localhost");
         ds.setDatabaseName("todoDB");
@@ -64,23 +77,120 @@ public class ToDoListMain {
         ds.setServerTimezone("EST");
         ds.setUseSSL(false);
         ds.setAllowPublicKeyRetrieval(true);
-        
+
         return ds;
     }
 
     private static void displayList() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try (Connection conn = ds.getConnection()) {
+            //DAO
+            Statement stmt = conn.createStatement();
+            String displayQuery = "SELECT * FROM todo";
+            ResultSet rs = stmt.executeQuery(displayQuery);
+
+            //VIEW
+            while (rs.next()) {
+                System.out.printf("%s: %s -- %s -- %s\n",
+                        rs.getString("id"),
+                        rs.getString("todo"),
+                        rs.getString("note"),
+                        rs.getBoolean("finished"));
+            }
+
+            System.out.println("");
+        }
     }
 
     private static void addItem() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //VIEW
+        System.out.println("Add Item");
+        System.out.println("What is the task?");
+        String task = sc.nextLine();
+        System.out.println("Any additional notes?");
+        String note = sc.nextLine();
+
+        //DAO
+        try (Connection conn = ds.getConnection()) {
+            String addQuery = "INSERT INTO todo(todo, note) VALUES(?,?)";
+            PreparedStatement pStmt = conn.prepareCall(addQuery);
+            pStmt.setString(1, task);
+            pStmt.setString(2, note);
+            pStmt.executeUpdate();
+            System.out.println("Add Complete");
+        }
     }
 
     private static void updateItem() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //VIEW
+        System.out.println("Update Item");
+        System.out.println("Which item do you want to update?");
+        String itemId = sc.nextLine();
+
+        //DAO
+        try (Connection conn = ds.getConnection()) {
+            String updateQuery = "SELECT * FROM todo WHERE id = ?";
+            PreparedStatement pStmt = conn.prepareCall(updateQuery);
+            pStmt.setString(1, itemId);
+            ResultSet rs = pStmt.executeQuery();
+            rs.next();
+            ToDo td = new ToDo();
+            td.setId(rs.getInt("id"));
+            td.setTodo(rs.getString("todo"));
+            td.setNote(rs.getString("note"));
+            td.setFinished(rs.getBoolean("finished"));
+
+            //VIEW
+            System.out.println("1. ToDo - " + td.getTodo());
+            System.out.println("2. Note - " + td.getNote());
+            System.out.println("3. Finished - " + td.isFinished());
+            System.out.println("What would you like to change?");
+
+            String choice = sc.nextLine();
+            switch (choice) {
+                case "1":
+                    System.out.println("Enter new ToDo:");
+                    String todo = sc.nextLine();
+                    td.setTodo(todo);
+                    break;
+                case "2":
+                    System.out.println("Enter new Note:");
+                    String note = sc.nextLine();
+                    td.setNote(note);
+                    break;
+                case "3":
+                    System.out.println("Toggling Finished to " + !td.isFinished());
+                    td.setFinished(!td.isFinished());
+                    break;
+                default:
+                    System.out.println("No change made");
+                    return;
+            }
+
+            //DAO
+            String updateSql = "UPDATE todo SET todo = ?, note = ?, finished = ? WHERE id = ?";
+            PreparedStatement updatePStmt = conn.prepareCall(updateSql);
+            updatePStmt.setString(1, td.getTodo());
+            updatePStmt.setString(2, td.getNote());
+            updatePStmt.setBoolean(3, td.isFinished());
+            updatePStmt.setInt(4, td.getId());
+            updatePStmt.executeUpdate();
+            System.out.println("Update Complete");
+        }
     }
 
     private static void removeItem() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //VIEW
+        System.out.println("Remove Item");
+        System.out.println("Which item would you like to remove?");
+        String itemId = sc.nextLine();
+        
+        //DAO
+        try(Connection conn = ds.getConnection()) {
+            String sql = "DELETE FROM todo WHERE id = ?";
+            PreparedStatement pStmt = conn.prepareCall(sql);
+            pStmt.setString(1, itemId);
+            pStmt.executeUpdate();
+            System.out.println("Remove Complete");
+        }    
     }
 }
