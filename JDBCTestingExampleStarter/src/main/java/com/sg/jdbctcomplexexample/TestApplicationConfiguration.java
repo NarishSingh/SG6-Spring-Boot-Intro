9378ee1c3de40adc6a523a622470e884976e842c
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-08-19 15:34:39 -0400

    completed SB/SMVC with JDBC and exception handling code along

--- JDBCTestingExampleStarter/src/main/java/com/sg/jdbctcomplexexample/TestApplicationConfiguration.java
+++ JDBCTestingExampleStarter/src/main/java/com/sg/jdbctcomplexexample/TestApplicationConfiguration.java
@@ -6,10 +6,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
-/**
- *
- * @author naris
- */
 @Configuration
 @ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, 
         value = CommandLineRunner.class))
