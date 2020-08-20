2e3dfd12399833d46d301fbbb71a11577820c4fb
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-08-20 13:33:08 -0400

    formatting

--- JDBCTestingExampleStarter/src/main/java/com/sg/jdbctcomplexexample/dao/RoomDao.java
+++ JDBCTestingExampleStarter/src/main/java/com/sg/jdbctcomplexexample/dao/RoomDao.java
@@ -8,9 +8,14 @@ import java.util.List;
  * @author kylerudy
  */
 public interface RoomDao {
+
     List<Room> getAllRooms();
+
     Room getRoomById(int id);
+
     Room addRoom(Room room);
+
     void updateRoom(Room room);
+
     void deleteRoomById(int id);
 }
