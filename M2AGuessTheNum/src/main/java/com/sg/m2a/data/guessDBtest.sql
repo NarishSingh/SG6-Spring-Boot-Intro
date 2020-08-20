ab763fe662eb0ff5ef5dbef6384b42b20784d262
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-08-20 11:46:31 -0400

    adding test db DDL

--- M2AGuessTheNum/src/main/java/com/sg/m2a/data/guessDBtest.sql
+++ M2AGuessTheNum/src/main/java/com/sg/m2a/data/guessDBtest.sql
@@ -6,3 +6,19 @@
 DROP DATABASE IF EXISTS guessDBtest;
 CREATE DATABASE guessDBtest;
 USE guessDBtest;
+
+CREATE TABLE game(
+    gameId INT PRIMARY KEY AUTO_INCREMENT,
+    answer CHAR(4) NOT NULL,
+    isFinished TINYINT(1) NOT NULL DEFAULT 0
+);
+
+CREATE TABLE round(
+    roundId INT PRIMARY KEY AUTO_INCREMENT,
+    gameId INT NOT NULL,
+    isCorrect TINYINT(1) NOT NULL DEFAULT 0,
+    digitMatches CHAR(7) NOT NULL,
+
+    CONSTRAINT `fk_Round_Game` FOREIGN KEY (gameId)
+        REFERENCES game (gameId)
+);
