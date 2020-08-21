bd16a31413482c794fec2109447d5471a6ad656f
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-08-20 21:06:21 -0400

    RoundDao testing suite drafted, GameDao suite started

--- M2AGuessTheNum/src/test/java/com/sg/m2a/data/GameDaoTest.java
+++ M2AGuessTheNum/src/test/java/com/sg/m2a/data/GameDaoTest.java
@@ -3,6 +3,8 @@ package com.sg.m2a.data;
 import com.sg.m2a.TestApplicationConfiguration;
 import com.sg.m2a.models.Game;
 import com.sg.m2a.models.Round;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -25,13 +27,75 @@ public class GameDaoTest {
     @Autowired
     RoundDao roundDao;
     
+    static Round r1;
+    static Round r1update;
+    static Round r2;
+    static Round r3;
+    static Game g1;
+    static Game g2;
+    
     public GameDaoTest() {
     }
     
     @BeforeClass
     public static void setUpClass() {
-        //test obj's
-        //TODO set up obj's
+        final int FIRST_GAME = 1;
+        final int SECOND_GAME = 2;
+        final String G1_ANSWER = "1234";
+        final String G2_ANSWER = "2345";
+        
+        final String FIRST_GUESS = "2345"; //correct for g2
+        final String FIRST_GUESS_UPDATE = "1111"; //fail, 1 exact for g1
+        final String SECOND_GUESS = "1234"; //correct for g1
+        
+        /*round obj's*/
+        //g1
+        r1 = new Round();
+        r1.setRoundId(1);
+        r1.setGameId(FIRST_GAME);
+        r1.setGuess(FIRST_GUESS);
+        r1.setTime(LocalDateTime.MIN);
+        r1.setDigitMatches("e:0:p:3"); //fail
+        
+        r1update = new Round();
+        r1update.setRoundId(1);
+        r1update.setGameId(FIRST_GAME);
+        r1update.setGuess(FIRST_GUESS_UPDATE);
+        r1update.setTime(LocalDateTime.MIN.plusMinutes(1));
+        r1update.setDigitMatches("e:1:p:0"); //fail
+        
+        r2 = new Round();
+        r2.setRoundId(2);
+        r2.setGameId(FIRST_GAME);
+        r2.setGuess(SECOND_GUESS);
+        r2.setTime(LocalDateTime.MIN.plusMinutes(2));
+        r2.setDigitMatches("e:4:p:0"); //pass
+        
+        //g2
+        r3 = new Round();
+        r3.setRoundId(3);
+        r3.setGameId(SECOND_GAME);
+        r3.setGuess(FIRST_GUESS);
+        r3.setTime(LocalDateTime.MIN);
+        r3.setDigitMatches("e:4:p:0"); //pass
+        
+        /*game obj's*/
+        g1 = new Game();
+        g1.setGameId(FIRST_GAME);
+        g1.setAnswer(G1_ANSWER);
+        g1.setIsFinished(false);
+        List<Round> g1Rounds = new ArrayList<>();
+        g1Rounds.add(r1);
+        g1Rounds.add(r2);
+        g1.setRounds(g1Rounds);
+        
+        g2 = new Game();
+        g2.setGameId(SECOND_GAME);
+        g2.setAnswer(G2_ANSWER);
+        g2.setIsFinished(false);
+        List<Round> g2Rounds = new ArrayList<>();
+        g2Rounds.add(r3);
+        g2.setRounds(g2Rounds);
     }
     
     @AfterClass
@@ -60,19 +124,12 @@ public class GameDaoTest {
      * Test of createGame method, of class GameDao.
      */
     @Test
-    public void testCreateGame() {
+    public void testCreateReadGame() {
         //arrange
         
-        
         //act
+        
         //assert
-    }
-
-    /**
-     * Test of readGameById method, of class GameDao.
-     */
-    @Test
-    public void testReadGameById() {
     }
 
     /**
@@ -80,6 +137,11 @@ public class GameDaoTest {
      */
     @Test
     public void testReadAllGames() {
+        //arrange
+        
+        //act
+        
+        //assert
     }
 
     /**
@@ -87,6 +149,11 @@ public class GameDaoTest {
      */
     @Test
     public void testUpdateGame() {
+        //arrange
+        
+        //act
+        
+        //assert
     }
 
     /**
@@ -94,6 +161,11 @@ public class GameDaoTest {
      */
     @Test
     public void testDeleteGameById() {
+        //arrange
+        
+        //act
+        
+        //assert
     }
 
     /**
@@ -101,6 +173,11 @@ public class GameDaoTest {
      */
     @Test
     public void testReadRoundsForGame() {
+        //arrange
+        
+        //act
+        
+        //assert
     }
 
 }
--- M2AGuessTheNum/src/test/java/com/sg/m2a/data/RoundDaoTest.java
+++ M2AGuessTheNum/src/test/java/com/sg/m2a/data/RoundDaoTest.java
@@ -3,6 +3,7 @@ package com.sg.m2a.data;
 import com.sg.m2a.TestApplicationConfiguration;
 import com.sg.m2a.models.Game;
 import com.sg.m2a.models.Round;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -25,13 +26,41 @@ public class RoundDaoTest {
     @Autowired
     RoundDao roundDao;
     
+    static Round r1;
+    static Round r1update;
+    static Round r2;
+    
     public RoundDaoTest() {
     }
     
     @BeforeClass
     public static void setUpClass() {
+        final int FIRST_GAME = 1; //assume answer is 1234
+        final String FIRST_GUESS = "2345"; //fail, 3 partials
+        final String FIRST_GUESS_UPDATE = "1111"; //fail, 1 exact
+        final String SECOND_GUESS = "1234"; //correct
+        
         //test obj's
-        //TODO set up obj's
+        r1 = new Round();
+        r1.setRoundId(1);
+        r1.setGameId(FIRST_GAME);
+        r1.setGuess(FIRST_GUESS);
+        r1.setTime(LocalDateTime.MIN);
+        r1.setDigitMatches("e:0:p:3");
+        
+        r1update = new Round();
+        r1update.setRoundId(1);
+        r1update.setGameId(FIRST_GAME);
+        r1update.setGuess(FIRST_GUESS_UPDATE);
+        r1update.setTime(LocalDateTime.MIN.plusMinutes(1)); //1 min after minimum
+        r1update.setDigitMatches("e:0:p:3");
+        
+        r2 = new Round();
+        r2.setRoundId(2);
+        r2.setGameId(FIRST_GAME);
+        r2.setGuess(SECOND_GUESS);
+        r2.setTime(LocalDateTime.MIN.plusMinutes(2));
+        r2.setDigitMatches("e:4:p:0");
     }
     
     @AfterClass
@@ -60,14 +89,15 @@ public class RoundDaoTest {
      * Test of createRound method, of class RoundDao.
      */
     @Test
-    public void testCreateRound() {
-    }
-
-    /**
-     * Test of readRoundById method, of class RoundDao.
-     */
-    @Test
-    public void testReadRoundById() {
+    public void testCreateReadRound() {
+        //arrange
+        Round r = roundDao.createRound(r1);
+        
+        //act
+        Round fromDao = roundDao.readRoundById(r1.getRoundId());
+        
+        //assert
+        assertEquals(r, fromDao);
     }
 
     /**
@@ -75,6 +105,17 @@ public class RoundDaoTest {
      */
     @Test
     public void testUpdateRound() {
+        //arrange
+        Round r = roundDao.createRound(r1);
+        Round original = roundDao.readRoundById(r.getRoundId());
+        
+        //act
+        roundDao.updateRound(r1update);
+        Round updated = roundDao.readRoundById(r1update.getRoundId());
+        
+        //assert
+        assertNotEquals(original, updated);
+        assertEquals(original.getRoundId(), updated.getRoundId());
     }
 
     /**
@@ -82,6 +123,18 @@ public class RoundDaoTest {
      */
     @Test
     public void testDeleteRoundByID() {
+        //arrange
+        Round round1 = roundDao.createRound(r1);
+        Round round2 = roundDao.createRound(r1);
+        
+        //act
+        roundDao.deleteRoundByID(round1.getRoundId());
+        Round r1FromDao = roundDao.readRoundById(round1.getRoundId());
+        Round r2FromDao = roundDao.readRoundById(round2.getRoundId());
+        
+        //assert
+        assertNull(r1FromDao);
+        assertNotNull(r2FromDao);
     }
 
 }
